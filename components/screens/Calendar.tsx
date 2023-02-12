75184fad30c83298768e625511b23aabf6f24573
nooreldeen <nooreldeen.osm@gmail.com>
2023-02-11 17:41:53 -0700

    Added a horribly styled calendar

--- components/screens/Calendar.tsx
+++ components/screens/Calendar.tsx
@@ -1,13 +1,43 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Calendar } from 'react-native-calendars';
 
-function HomeScreen() {
+function CalendarScreen() {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
-
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
+            <Calendar
+                style={{
+                    borderRadius: 10,
+                    margin: 10,
+                    backgroundColor: '#3A3B3C',
+                    paddingBottom: 10,
+                }}
+                theme={{
+                    backgroundColor: '#000',
+                    calendarBackground: '#ffffff',
+                    textSectionTitleColor: '#b6c1cd',
+                    textSectionTitleDisabledColor: '#d9e1e8',
+                    selectedDayBackgroundColor: '#00adf5',
+                    selectedDayTextColor: '#ffffff',
+                    todayTextColor: '#00adf5',
+                    dayTextColor: '#2d4150',
+                    textDisabledColor: '#d9e1e8',
+                    dotColor: '#00adf5',
+                    selectedDotColor: '#ffffff',
+                    arrowColor: 'orange',
+                    disabledArrowColor: '#d9e1e8',
+                    monthTextColor: 'blue',
+                    indicatorColor: 'blue',
+                    textDayFontWeight: '300',
+                    textMonthFontWeight: 'bold',
+                    textDayHeaderFontWeight: '300',
+                    textDayFontSize: 16,
+                    textMonthFontSize: 16,
+                    textDayHeaderFontSize: 16
+                }}></Calendar>
+        </SafeAreaView >
     );
-}
+};
 
-export default HomeScreen;
+
+export default CalendarScreen;
