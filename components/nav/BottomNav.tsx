4273e8996c70e2ab370f2e406e001c70386259cf
nooreldeen <nooreldeen.osm@gmail.com>
2023-02-18 19:29:24 -0700

    Style Update
    Updates the calendar and nav styles

--- components/nav/BottomNav.tsx
+++ components/nav/BottomNav.tsx
@@ -1,23 +1,54 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import Calendar from '../screens/Calendar';
 import Entries from '../screens/Entries';
 import More from '../screens/More';
 import Stats from '../screens/Stats';
 import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 function BottomNav() {
     return (
-        <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Calendar" component={Calendar} />
-                <Tab.Screen name="Entries" component={Entries} />
-                <Tab.Screen name="Stats" component={Stats} />
-                <Tab.Screen name="More" component={More} />
-            </Tab.Navigator>
-        </NavigationContainer>
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
+            <NavigationContainer >
+                <Tab.Navigator
+                    barStyle={{ backgroundColor: '#000', paddingBottom: 15 }}
+                    initialRouteName="Calendar"
+                    activeColor="#e91e63">
+                    <Tab.Screen name="Calendar" component={Calendar}
+                        options={{
+                            tabBarLabel: 'Calendar',
+                            tabBarIcon: ({ color }) => (
+                                <MaterialCommunityIcons name="calendar" color={color} size={26} />
+                            ),
+                        }} />
+                    <Tab.Screen name="Entries" component={Entries}
+                        options={{
+                            tabBarLabel: 'Entries',
+                            tabBarIcon: ({ color }) => (
+                                <MaterialCommunityIcons name="book" color={color} size={26} />
+                            ),
+                        }} />
+                    <Tab.Screen name="Stats" component={Stats}
+                        options={{
+                            tabBarLabel: 'Stats',
+                            tabBarIcon: ({ color }) => (
+                                <MaterialCommunityIcons name="chart-arc" color={color} size={26} />
+                            ),
+                        }} />
+                    <Tab.Screen name="More" component={More}
+                        options={{
+                            tabBarLabel: 'More',
+                            tabBarIcon: ({ color }) => (
+                                <MaterialCommunityIcons name="cog" color={color} size={26} />
+                            ),
+                        }} />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </SafeAreaView>
     );
 }
 
--- components/screens/Calendar.tsx
+++ components/screens/Calendar.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+    StyleSheet,
+    Text,
+    SafeAreaView,
+    ScrollView,
+    View,
+    StatusBar,
+} from 'react-native';
 import { Calendar } from 'react-native-calendars';
 
 function CalendarScreen() {
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
+
+        <ScrollView style={{ backgroundColor: '#000' }}>
             <Calendar
                 style={{
                     borderRadius: 10,
@@ -13,29 +21,17 @@ function CalendarScreen() {
                     paddingBottom: 10,
                 }}
                 theme={{
-                    backgroundColor: '#000',
-                    calendarBackground: '#ffffff',
-                    textSectionTitleColor: '#b6c1cd',
-                    textSectionTitleDisabledColor: '#d9e1e8',
-                    selectedDayBackgroundColor: '#00adf5',
-                    selectedDayTextColor: '#ffffff',
-                    todayTextColor: '#00adf5',
-                    dayTextColor: '#2d4150',
-                    textDisabledColor: '#d9e1e8',
-                    dotColor: '#00adf5',
-                    selectedDotColor: '#ffffff',
-                    arrowColor: 'orange',
-                    disabledArrowColor: '#d9e1e8',
-                    monthTextColor: 'blue',
-                    indicatorColor: 'blue',
-                    textDayFontWeight: '300',
-                    textMonthFontWeight: 'bold',
-                    textDayHeaderFontWeight: '300',
-                    textDayFontSize: 16,
-                    textMonthFontSize: 16,
-                    textDayHeaderFontSize: 16
+                    backgroundColor: '#3A3B3C',
+                    calendarBackground: '#3A3B3C',
+                    textSectionTitleColor: 'white',
+                    textDayHeaderFontSize: 13,
+                    monthTextColor: '#fff',
+                    dayTextColor: '#fff',
+                    textMonthFontSize: 30,
+                    textDayFontSize: 20,
+                    textDisabledColor: '#0000'
                 }}></Calendar>
-        </SafeAreaView >
+        </ScrollView>
     );
 };
 
